a5dced42013c53c257bd6053fc94eea86a3557e4
Albert Iskandarov <a.iskandarov@gmail.com>
2018-09-20 01:36:58 +0900

    fourth commit

---
+++ ContosoSite/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoSite.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return  View();
+        }
+
+        public ActionResult Welcome(string name, int numTimes = 4)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+            //return HttpUtility.HtmlEncode("Hello" + name + " , ID =" + numTimes);
+        }
+    }
+}
